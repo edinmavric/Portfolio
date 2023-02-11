58c5e1b0f22d5bfc9e4a5f3976a76d857247279c
edinmavric <edinmavric10@gmail.com>
2023-02-11 12:50:26 +0100

    added active class on header

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,10 +1,3 @@
-// const f = document.getElementById('foo');
-
-// document.addEventListener('click', (ev) => {
-//   f.style.transform = `translateY(${ev.clientY - 25}px)`;
-//   f.style.transform += `translateX(${ev.clientX - 25}px)`;
-// }, false);
-
 let prevScrollpos = window.pageYOffset;
 
 window.onscroll = headerMove = () => {
@@ -12,9 +5,25 @@ let currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("hide-header").style.top = "0";
   } else {
-    document.getElementById("hide-header").style.top = "-100px";
+    document.getElementById("hide-header").style.top = "-90px";
   }
   prevScrollpos = currentScrollPos;
 }
 
 window.onpagehide
+
+let navContainer = document.getElementById('navMenus');
+let btns = navContainer.getElementsByClassName("btns");
+
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    let current = document.getElementsByClassName("active");
+
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(" active", "");
+    }
+
+    this.className += " active";
+  });
+}
+
