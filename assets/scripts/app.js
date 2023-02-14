d547670419587be591f79bc1e20424dc52c95f42
edinmavric <edinmavric10@gmail.com>
2023-02-14 20:56:57 +0100

    added scroll animation

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -32,3 +32,18 @@ const preloader = document.querySelector(".preloader");
 window.addEventListener("load", function () {
   preloader.classList.add("hide-preloader");
 });
+
+function reveal() {
+  let reveals = document.querySelectorAll(".reveal");
+  for (let i = 0; i < reveals.length; i++) {
+    let windowHeight = window.innerHeight;
+    let elementTop = reveals[i].getBoundingClientRect().top;
+    let elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    }
+  }
+}
+window.addEventListener("scroll", reveal);
+
+reveal();
