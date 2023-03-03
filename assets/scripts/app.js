f0c30f67c1d2b8db793a7144bd0184cf07c964ab
edinmavric <edinmavric10@gmail.com>
2023-03-03 18:28:42 +0100

    added mouse trail effect

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -125,3 +125,33 @@ window.onclick = function (event) {
 // } else if (!sidebar.style.display == "none") {
 //   body.style.overflowY = "auto";
 // }
+const cursor = document.querySelector('.cursor');
+
+let mouseX = 0;
+let mouseY = 0;
+
+let cursorX = 0;
+let cursorY = 0;
+
+let speed = 0.5; // change to increase the ease
+
+function animate() {
+    let distX = mouseX - cursorX;
+    let distY = mouseY - cursorY;
+
+    cursorX = cursorX + (distX * speed);
+    cursorY = cursorY + (distY * speed);
+
+    cursor.style.left = cursorX + 'px';
+    cursor.style.top = cursorY + 'px';
+
+    requestAnimationFrame(animate);
+}
+
+
+animate();
+
+document.addEventListener('mousemove', (event) => {
+    mouseX = event.pageX;
+    mouseY = event.pageY;
+})
