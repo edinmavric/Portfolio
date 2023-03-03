c0d30abc581485b0792a5340f2ac09aec74af46c
edinmavric <edinmavric10@gmail.com>
2023-03-03 21:12:23 +0100

    code reconstruction

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -126,57 +126,55 @@ window.onclick = function (event) {
 //   body.style.overflowY = "auto";
 // }
 
+let dots = [],
+  mouse = {
+    x: 0,
+    y: 0,
+  };
 
-   let dots = [],
-   mouse = {
-     x: 0,
-     y: 0
-   };
-
-let Dot = function() {
- this.x = 0;
- this.y = 0;
- this.node = (function(){
-   let n = document.createElement("div");
-   n.className = "trail";
-   document.body.appendChild(n);
-   return n;
- }());
+let Dot = function () {
+  this.x = 0;
+  this.y = 0;
+  this.node = (function () {
+    let n = document.createElement("div");
+    n.className = "trail";
+    document.body.appendChild(n);
+    return n;
+  })();
 };
-Dot.prototype.draw = function() {
- this.node.style.left = this.x + "px";
- this.node.style.top = this.y + "px";
+Dot.prototype.draw = function () {
+  this.node.style.left = this.x + "px";
+  this.node.style.top = this.y + "px";
 };
 
 for (let i = 0; i < 12; i++) {
- let d = new Dot();
- dots.push(d);
+  let d = new Dot();
+  dots.push(d);
 }
 
 function draw() {
- let x = mouse.x,
-     y = mouse.y;
- 
- dots.forEach(function(dot, index, dots) {
-   let nextDot = dots[index + 1] || dots[0];
-   
-   dot.x = x;
-   dot.y = y;
-   dot.draw();
-   x += (nextDot.x - dot.x) * .6;
-   y += (nextDot.y - dot.y) * .6;
+  let x = mouse.x,
+    y = mouse.y;
 
- });
+  dots.forEach(function (dot, index, dots) {
+    let nextDot = dots[index + 1] || dots[0];
+
+    dot.x = x;
+    dot.y = y;
+    dot.draw();
+    x += (nextDot.x - dot.x) * 0.6;
+    y += (nextDot.y - dot.y) * 0.6;
+  });
 }
 
-addEventListener("mousemove", function(event) {
- mouse.x = event.pageX;
- mouse.y = event.pageY;
+addEventListener("mousemove", function (event) {
+  mouse.x = event.pageX;
+  mouse.y = event.pageY;
 });
 
 function animate() {
- draw();
- requestAnimationFrame(animate);
+  draw();
+  requestAnimationFrame(animate);
 }
 
 animate();
