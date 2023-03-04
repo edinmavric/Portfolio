10e3da522e7d0f057065e91e7bea50e3ddd11654
edinmavric <edinmavric10@gmail.com>
2023-03-04 13:21:31 +0100

    fixed page responsivity

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -178,3 +178,14 @@ function animate() {
 }
 
 animate();
+
+
+var docWidth = document.documentElement.offsetWidth;
+[].forEach.call(
+  document.querySelectorAll('*'),
+  function(el) {
+    if (el.offsetWidth > docWidth) {
+      console.log(el);
+    }
+  }
+);
