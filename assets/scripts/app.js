9666a20263f6e6515b1c2f280fac40b03f721198
Edin Mavric <edinmavric10@gmail.com>
2023-05-13 10:23:15 +0200

    updated resume

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,120 +1,106 @@
 let body = document.body;
 
-// Hiding header and following through the whole page with CSS help of sticky
-
 let prevScrollpos = window.pageYOffset;
-let hideHeader = document.getElementById("hide-header");
+let hideHeader = document.getElementById('hide-header');
 
 window.onscroll = headerMove = () => {
   let currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    hideHeader.style.top = "0";
+    hideHeader.style.top = '0';
   } else {
-    hideHeader.style.top = "-100px";
+    hideHeader.style.top = '-100px';
   }
   prevScrollpos = currentScrollPos;
 };
 
 window.onpagehide;
 
-// Header active class on pressed header component
-
-let navContainer = document.getElementById("navMenus");
-let btns = navContainer.getElementsByClassName("btns");
+let navContainer = document.getElementById('navMenus');
+let btns = navContainer.getElementsByClassName('btns');
 
 for (let i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", () => {
-    let current = document.getElementsByClassName("active");
+  btns[i].addEventListener('click', () => {
+    let current = document.getElementsByClassName('active');
 
     if (current.length > 0) {
-      current[0].className = current[0].className.replace(" active", "");
+      current[0].className = current[0].className.replace(' active', '');
     }
 
-    this.className += " active";
+    this.className += ' active';
     setTimeout(() => {
-      hideHeader.style.top = "-90px";
+      hideHeader.style.top = '-90px';
     }, 800);
   });
 }
 
-// Preloader logic settings
+const preloader = document.querySelector('.preloader');
 
-const preloader = document.querySelector(".preloader");
-
-window.addEventListener("load", () => {
-  preloader.classList.add("hide-preloader");
-  body.classList.remove("invisible");
+window.addEventListener('load', () => {
+  preloader.classList.add('hide-preloader');
+  body.classList.remove('invisible');
 });
 
-// Reveal site sections on scroll
-
 function reveal() {
-  let reveals = document.querySelectorAll(".reveal");
+  let reveals = document.querySelectorAll('.reveal');
   for (let i = 0; i < reveals.length; i++) {
     let windowHeight = window.innerHeight;
     let elementTop = reveals[i].getBoundingClientRect().top;
     let elementVisible = 130;
     if (elementTop < windowHeight - elementVisible) {
-      reveals[i].classList.add("active");
+      reveals[i].classList.add('active');
     }
   }
 }
-window.addEventListener("scroll", reveal);
+window.addEventListener('scroll', reveal);
 
 reveal();
 
-// Show More and Show Less button on cards
+let showBtn = document.getElementById('showMLBtn');
+let invisibleCards = document.querySelector('.invisCards');
 
-let showBtn = document.getElementById("showMLBtn");
-let invisibleCards = document.querySelector(".invisCards");
-
-showBtn.addEventListener("click", () => {
-  showBtn.classList.add("font");
-  if (invisibleCards.className === "invisCards invisible") {
-    invisibleCards.className = "invisCards visible";
-    showBtn.innerHTML = "Show Less...";
+showBtn.addEventListener('click', () => {
+  showBtn.classList.add('font');
+  if (invisibleCards.className === 'invisCards invisible') {
+    invisibleCards.className = 'invisCards visible';
+    showBtn.innerHTML = 'Show Less...';
   } else {
-    invisibleCards.className = "invisCards invisible";
-    showBtn.innerHTML = "Show More...";
+    invisibleCards.className = 'invisCards invisible';
+    showBtn.innerHTML = 'Show More...';
   }
 });
 
-// Header responsive
-
-let sidebarShowBtn = document.querySelector(".sidebar-open-btn");
-let closeButton = document.getElementsByClassName("close")[0];
-let sidebar = document.getElementById("myModal");
-let btnsside = document.querySelectorAll(".btnsside");
+let sidebarShowBtn = document.querySelector('.sidebar-open-btn');
+let closeButton = document.getElementsByClassName('close')[0];
+let sidebar = document.getElementById('myModal');
+let btnsside = document.querySelectorAll('.btnsside');
 
 for (let i = 0; i < btnsside.length; i++) {
-  btnsside[i].addEventListener("click", () => {
+  btnsside[i].addEventListener('click', () => {
     setTimeout(() => {
-      hideHeader.style.top = "-90px";
+      hideHeader.style.top = '-90px';
     }, 800);
   });
 
   for (let i = 0; i < btnsside.length; i++) {
-    btnsside[i].addEventListener("click", () => {
-      sidebar.style.display = "none";
+    btnsside[i].addEventListener('click', () => {
+      sidebar.style.display = 'none';
     });
   }
 }
 
 sidebarShowBtn.onclick = function () {
-  sidebar.style.display = "block";
+  sidebar.style.display = 'block';
 };
 
 closeButton.onclick = function () {
-  sidebar.style.display = "none";
+  sidebar.style.display = 'none';
 };
 
 window.onclick = function (event) {
   if (event.target == sidebar) {
-    sidebar.style.display = "none";
+    sidebar.style.display = 'none';
   }
 };
-
-// Mouse trailing effect
 
 let dots = [],
   mouse = {
@@ -126,15 +112,15 @@ let Dot = function () {
   this.x = 0;
   this.y = 0;
   this.node = (function () {
-    let n = document.createElement("div");
-    n.className = "trail";
+    let n = document.createElement('div');
+    n.className = 'trail';
     document.body.appendChild(n);
     return n;
   })();
 };
 Dot.prototype.draw = function () {
-  this.node.style.left = this.x + "px";
-  this.node.style.top = this.y + "px";
+  this.node.style.left = this.x + 'px';
+  this.node.style.top = this.y + 'px';
 };
 
 for (let i = 0; i < 12; i++) {
@@ -157,7 +143,7 @@ function draw() {
   });
 }
 
-addEventListener("mousemove", function (event) {
+addEventListener('mousemove', function (event) {
   mouse.x = event.pageX;
   mouse.y = event.pageY;
 });
@@ -170,20 +156,20 @@ function animate() {
 animate();
 
 let docWidth = document.documentElement.offsetWidth;
-[].forEach.call(document.querySelectorAll("*"), function (el) {
+[].forEach.call(document.querySelectorAll('*'), function (el) {
   if (el.offsetWidth > docWidth) {
     console.log(el);
   }
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-  let progressBarInner = document.querySelector(".progress-bar-inner");
+document.addEventListener('DOMContentLoaded', function () {
+  let progressBarInner = document.querySelector('.progress-bar-inner');
 
-  window.addEventListener("scroll", function () {
+  window.addEventListener('scroll', function () {
     let h = document.documentElement;
     let st = h.scrollTop || document.body.scrollTop;
     let sh = h.scrollHeight || document.body.scrollHeight;
     let percent = (st / (sh - h.clientHeight)) * 100;
-    progressBarInner.style.width = percent + "%";
+    progressBarInner.style.width = percent + '%';
   });
 });
